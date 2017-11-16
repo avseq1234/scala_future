a08f66c848e3d26b9db19b6c95c1581cec0d5102
avseq1234 <sun.stef@gmail.com>
2017-11-16 09:45:50 +0800

    add comment for f4

--- src/com/scala/future/demo/FutureSequenceDemo.scala
+++ src/com/scala/future/demo/FutureSequenceDemo.scala
@@ -12,11 +12,15 @@ import scala.util.{Failure, Success}
   */
 object FutureSequenceDemo {
 
-  def computation(): Int = { 25 + 50 }
+  def computation(): Int = {
+    25 + 50
+  }
 
 
   def main(args: Array[String]): Unit = {
-    val theFuture = Future { computation() }
+    val theFuture = Future {
+      computation()
+    }
     theFuture.onComplete {
       case Success(result) => println(result)
       case Failure(t) => println(s"Error: %{t.getMessage}")
@@ -26,28 +30,36 @@ object FutureSequenceDemo {
       TimeUnit.SECONDS.sleep(1)
       "f1"
     }
+
     val f2 = Future {
       TimeUnit.SECONDS.sleep(2)
       "f2"
     }
+
     val f3 = Future {
       TimeUnit.SECONDS.sleep(3)
       2342
     }
 
-        val f5: Future[(String, String, Int)] =
-          for {
-            r2 <- f2
-            r3 <- f3
-            r1 <- f1
-          } yield (r1, r2, r3)
+
+    //
+    //
+    // In the reality, we need to send request to different resources and process when they all back together
+    //
+    //
+    val f5: Future[(String, String, Int)] =
+      for {
+        r2 <- f2
+        r3 <- f3
+        r1 <- f1
+      } yield (r1, r2, r3)
 
     //
     // f5 is identical with f4
     //
     val f4 = Future.sequence(Seq(f1, f2, f3))
-    val results= Await.result(f4, 4.seconds)
-      val result1s= Await.result(f5, 4.seconds)
+    val results = Await.result(f4, 4.seconds)
+    val result1s = Await.result(f5, 4.seconds)
     println(results)
     println(result1s)
   }
